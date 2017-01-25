c7a45542ca4a8da75c0a632ea384faf9d75f00bd
Karl Wallenius <karl.wallenius@gmail.com>
2017-01-25 01:29:47 +0100

    Adde classes for getting host name/ipaddress and tests

---
+++ Kaguwa.Commands.Network.Tests/GetNetworkHostCommandTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Management.Automation;
+
+namespace Kaguwa.Commands.Network.Tests
+{
+    [TestClass]
+    public class GetNetworkHostCommandTests
+    {
+        [TestMethod]
+        public void ShouldCreateCmdlet()
+        {
+            // Create an instance of the Cmdlet.
+            GetNetworkHostCommand cmd = new GetNetworkHostCommand();
+
+            // Check if it is a Cmdlet.
+            Assert.IsTrue(cmd is Cmdlet);
+        }
+    }
+}
