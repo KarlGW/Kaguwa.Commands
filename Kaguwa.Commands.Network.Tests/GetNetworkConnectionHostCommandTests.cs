33adf39c16d58ae30a0361de442fc4e17e5b067c
Karl Wallenius <karl.wallenius@gmail.com>
2017-02-01 23:14:50 +0100

    renamed

---
+++ Kaguwa.Commands.Network.Tests/GetNetworkConnectionHostCommandTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Management.Automation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kaguwa.Commands.Network.Tests
+{
+    [TestClass]
+    public class GetNetworkConnectionHostCommandTests
+    {
+        [TestMethod]
+        public void ShouldCreateCmdlet()
+        {
+            // Create an instance of the Cmdlet.
+            GetNetworkConnectionHostCommand cmd = new GetNetworkConnectionHostCommand();
+
+            // Check if it is a Cmdlet.
+            Assert.IsTrue(cmd is Cmdlet);
+        }
+    }
+}
