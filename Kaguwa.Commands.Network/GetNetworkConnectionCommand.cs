ccc380a739850adce3290efcec901c84e7314539
Karl Wallenius <karl.wallenius@gmail.com>
2017-02-01 23:14:09 +0100

    Fixed #2, and added parameter sets

--- Kaguwa.Commands.Network/GetNetworkConnectionCommand.cs
+++ Kaguwa.Commands.Network/GetNetworkConnectionCommand.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections.Generic;
 using System.Management.Automation;
 using System.Linq;
-using System.Text.RegularExpressions;
 using Kaguwa.Commands.Network.Types;
 
 namespace Kaguwa.Commands.Network
 {
-    [Cmdlet(VerbsCommon.Get, "NetworkConnection")]
+    [Cmdlet(VerbsCommon.Get, "NetworkConnection",
+        DefaultParameterSetName = "ProcessName")]
     [OutputType(typeof(NetworkConnection))]
     public class GetNetworkConnectionCommand : Cmdlet
     {
-        // Define parameters.
+        #region Parameters
+
         [Parameter(
             Position = 0,
+            ParameterSetName = "ProcessName",
             ValueFromPipeline = true,
-            ValueFromPipelineByPropertyName = true)]
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The name of one or more proccess in network connection to filter by.")]
         [Alias("Name", "Process")]
         public string[] ProcessName
         {
@@ -24,7 +27,10 @@ namespace Kaguwa.Commands.Network
         private string[] processName;
 
         [Parameter(
-            ValueFromPipelineByPropertyName = true)]
+            Position = 0,
+            ParameterSetName = "ProcessId",
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The ID of one or more proccess in network connection to filter by.")]
         [Alias("Id", "PID")]
         public int[] ProcessId
         {
@@ -33,8 +39,9 @@ namespace Kaguwa.Commands.Network
         }
         private int[] processId;
 
-        [Parameter(Position = 1)]
-        [Alias("Connection")]
+        [Parameter(
+            Position = 1,
+            HelpMessage = "The state of connections to filter by.")]
         [ValidateSet(
             "Established",
             "Listening",
@@ -48,8 +55,11 @@ namespace Kaguwa.Commands.Network
         }
         private string state;
 
+        #endregion Parameters
+
         private IEnumerable<NetworkConnection> _connections;
 
+        #region CmdletOverrides
         protected override void BeginProcessing()
         {
             base.BeginProcessing();
@@ -65,24 +75,26 @@ namespace Kaguwa.Commands.Network
             // Either use ProcessName or ProcessId.
             if (ProcessName != null)
             {
-                connections = connections.Where(connection => connection.ProcessName != null &&
-                                                Regex.IsMatch(connection.ProcessName,
-                                                string.Format("^(?:{0})", string.Join("|", ProcessName))));
+                connections = ConnectionByProcessName(connections, ProcessName);                
             }
             else if (ProcessId != null)
             {
-                connections = connections.Where(connection => ProcessId.Contains(connection.ProcessId));
+                connections = ConnectionByProcessId(connections, ProcessId);
             }
 
             // Check if state was specified.
             if (State != null)
             {
-                connections = connections.Where(connection => connection.State == State);
+                connections = ConnectionByState(connections, State);
             }
 
             // Write the NetworkConnection objects.
             connections.ToList().ForEach(WriteObject);
         }
+
+        #endregion CmdletOverrides
+
+        #region Helper Methods
 
         /// <summary>
         /// Static internal method to get the connections in use by the localhost.
@@ -97,5 +109,74 @@ namespace Kaguwa.Commands.Network
 
             return connections;
         }
+
+        /// <summary>
+        /// Helper method to filter connections from GetConnections().
+        /// </summary>
+        /// <param name="connections">IEnumerable containing TCP/UDP connections.</param>
+        /// <param name="ProcessName">Parameter ProcessName</param>
+        /// <returns>
+        /// It returns an IEnumerable containing filtered TCP/UDP connections.
+        /// </returns>
+        private static IEnumerable<NetworkConnection> ConnectionByProcessName(IEnumerable<NetworkConnection> connections, string[] ProcessName)
+        {
+            // Define wildcard
+            List<NetworkConnection> filteredConnections = new List<NetworkConnection>();
+
+            foreach (string name in ProcessName)
+            {
+
+                WildcardOptions wildcardOptions = WildcardOptions.IgnoreCase |
+                    WildcardOptions.Compiled;
+
+                WildcardPattern wildcard = new WildcardPattern(name, wildcardOptions);
+
+                foreach (NetworkConnection connection in connections)
+                {
+
+                    if (wildcard.IsMatch(connection.ProcessName))
+                    {
+                        filteredConnections.Add(connection);
+                    }
+                }
+            }
+
+            connections = null;
+            connections = filteredConnections;
+
+            return connections;
+        }
+
+        /// <summary>
+        /// Helper method to filter connections from GetConnections().
+        /// </summary>
+        /// <param name="connections">IEnumerable containing TCP/UDP connections.</param>
+        /// <param name="ProcessId">Parameter ProcessId</param>
+        /// <returns>
+        /// It returns an IEnumerable containing filtered TCP/UDP connections.
+        /// </returns>
+        private static IEnumerable<NetworkConnection> ConnectionByProcessId(IEnumerable<NetworkConnection> connections, int[] ProcessId)
+        {
+            connections = connections.Where(connection => ProcessId.Contains(connection.ProcessId));
+
+            return connections;
+        }
+
+        /// <summary>
+        /// Helper method to filter connections from GetConnections().
+        /// </summary>
+        /// <param name="connections">IEnumerable containing TCP/UDP connections.</param>
+        /// <param name="State">Parameter ProcessName</param>
+        /// <returns>
+        /// It returns an IEnumerable containing filtered TCP/UDP connections.
+        /// </returns>
+        private static IEnumerable<NetworkConnection> ConnectionByState(IEnumerable<NetworkConnection> connections, string State)
+        {
+            connections = connections.Where(connection => connection.State == State);
+
+            return connections;
+        }
+
+        #endregion Helper Methods
     }
 }
